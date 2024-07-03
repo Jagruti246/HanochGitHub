3a33141ef350b98946d75dbb6e763e176e076e31
Jagruti Priyadarshini <jagrutipriyadarshini246@gmail.com>
2024-07-03 08:06:32 +0530

    navbar

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 
 //=========================================MAIN-JS==================================
-window.addEventListener('scroll', function() {
-  var scroll = window.scrollY;
+// window.addEventListener('scroll', function() {
+//   var scroll = window.scrollY;
 
-  if (scroll >= 10) {
-      document.querySelector('.navbar').classList.add('nav-scroll');
-      document.querySelector('.back_to_top').classList.add('show_btn');
-  } else {
-      document.querySelector('.navbar').classList.remove('nav-scroll');
-      document.querySelector('.back_to_top').classList.remove('show_btn');
-  }
-});
+//   if (scroll >= 0) {
+//       document.querySelector('.navbar').classList.add('nav-scroll');
+//       // document.querySelector('.back_to_top').classList.add('show_btn');
+//   } else {
+//       // document.querySelector('.navbar').classList.remove('nav-scroll');
+//       // document.querySelector('.back_to_top').classList.remove('show_btn');
+//   }
+// });
+
+
 
 
 
@@ -22,27 +24,28 @@ window.addEventListener('scroll', function() {
 //=================================SLICK-JS========================================
 
 $('.testimonial_slider').slick({
-    dots: true,
-    arrows: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll:1,
+    dots: true,           // Enable dots for navigation
+    arrows: false,         // Disable arrow navigation
+    infinite: true,         // Enable infinite looping
+
+    speed: 300,               // Transition speed in milliseconds                           
+    slidesToShow: 2,            // Number of slides to show at once                               
+    slidesToScroll:1,               // Number of slides to scroll at once                         
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1024,        // Settings for screens below 1024 pixels                              
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
+          slidesToShow: 2,            // Settings for screens below 768 pixels                                
+          slidesToScroll: 1,                                       
+          infinite: true,                                              
+          dots: true                                                      
         }
       },
       {
-        breakpoint: 768,
+        breakpoint: 768,                                    
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToShow: 1,                                                  
+          slidesToScroll: 1                                                
         }
       },
     
